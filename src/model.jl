d391e1ef600c2315ccb4d782783e465ab69eda25
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2023-04-13 09:24:46 +0200

    Use standard functions for general constraints.

--- src/model.jl
+++ src/model.jl
@@ -6,12 +6,6 @@ Set all constraints for a `Storage`. Can serve as fallback option for all unspec
 subtypes of `Storage`.
 """
 function EMB.create_node(m, n::CO2Storage, 𝒯, 𝒫, modeltype::EnergyModel)
-    # 𝒫ᵃᵈᵈ   = setdiff(keys(n.Input), [CO2])
-
-    # Constraint for additional required input.
-    # @constraint(m, [t ∈ 𝒯, p ∈ 𝒫ᵃᵈᵈ], 
-        # m[:flow_in][n, t, p] == m[:flow_in][n, t, CO2] * n.Input[p])
-
 
     p_stor = n.Stor_res
     # 𝒫ᵉᵐ    = EMB.res_sub(𝒫, ResourceEmit)
@@ -44,7 +38,7 @@ function EMB.create_node(m, n::CO2Storage, 𝒯, 𝒫, modeltype::EnergyModel)
             )
         end
     end
-    
+
     # Constraint for the other emissions to avoid problems with unconstrained variables.
     @constraint(m, [t ∈ 𝒯, p_em ∈ keys(n.Output)],
         m[:emissions_node][n, t, p_em] == 0)
@@ -53,27 +47,13 @@ function EMB.create_node(m, n::CO2Storage, 𝒯, 𝒫, modeltype::EnergyModel)
     @constraint(m, [t ∈ 𝒯, p ∈ keys(n.Output)],
          m[:flow_out][n, t, p] == 0)
 
-    # Constraint for storage rate use
-    @constraint(m, [t ∈ 𝒯],
-        m[:stor_rate_use][n, t] == m[:flow_in][n, t, p_stor]
-    )
+    # Constraint for storage rate use, and use of additional required input resources.
+    EMB.constraints_flow_in(m, n, 𝒯)
 
-    @constraint(m, [t ∈ 𝒯],
-        m[:stor_rate_use][n, t] <= m[:stor_rate_inst][n, t]
-    )
+    # Bounds for the storage level and storage rate used.
+    EMB.constraints_capacity(m, n, 𝒯)
 
-    @constraint(m, [t ∈ 𝒯],
-        m[:stor_level][n, t] <= m[:stor_cap_inst][n, t]
-    )
-
-    @constraint(m, [t_inv ∈ 𝒯ᴵⁿᵛ],
-        m[:opex_fixed][n, t_inv] == 
-            n.Opex_fixed[t_inv] * m[:stor_cap_inst][n, first(t_inv)]
-    )
-
-    @constraint(m, [t_inv ∈ 𝒯ᴵⁿᵛ],
-        m[:opex_var][n, t_inv] == 
-            sum(m[:flow_in][n, t , p_stor] * n.Opex_var[t] * t.duration for t ∈ t_inv)
-    )
+    EMB.constraints_opex_fixed(m, n, 𝒯ᴵⁿᵛ)
+    EMB.constraints_opex_var(m, n, 𝒯ᴵⁿᵛ)
 
 end
